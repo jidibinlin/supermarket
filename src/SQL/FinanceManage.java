1bb59685322ead047324ba07aea4cfe71a0636a8
zyz <756259772@qq.com>
2019-01-07 11:47:22 +0800

    test

--- src/SQL/FinanceManage.java
+++ src/SQL/FinanceManage.java
@@ -1,16 +1,18 @@
 package SQL;
 
 import JDBC.connect;
+
 import java.sql.*;
+
 import SQL.*;
 //import SQLwords.java
 
 public class FinanceManage {
-    Statement stmt=null;
-    ResultSet rs=null;
+    Statement stmt = null;
+    ResultSet rs = null;
     String seResult;
     String result;
-    SQLwords sql=new SQLwords();
+    SQLwords sql = new SQLwords();
 //    //insert update delete模板
 //    public void (){
 //        try {
@@ -45,140 +47,220 @@ public class FinanceManage {
     //销量管理
 
     //销量统计
-    public void selesVolumeCollect(){
+    public void selesVolumeCollect() {
         try {
             stmt = connect.getConnection().createStatement();
-            rs=stmt.executeQuery(  "select Product.proNum,proName,counts "+
-		                            "from Product,"+
-			                                "(select Shopping.proNum,sum(number) counts "+
-				                            "from Shopping "+
-                                            "group by proNum) AS PRO "+
-		                            "where PRO.proNum=Product.proNum;");
-            while(rs.next()){
-                System.out.println("proNum "+rs.getString("proNum")+"proName "+rs.getString("proName")+"counts: "+rs.getString("counts"));
+            rs = stmt.executeQuery("select Product.proNum,proName,counts " +
+                    "from Product," +
+                    "(select Shopping.proNum,sum(number) counts " +
+                    "from Shopping " +
+                    "group by proNum) AS PRO " +
+                    "where PRO.proNum=Product.proNum;");
+            while (rs.next()) {
+                System.out.println("proNum " + rs.getString("proNum") + "proName " + rs.getString("proName") + "counts: " + rs.getString("counts"));
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally{
-            connect.release(stmt,rs);
+        } finally {
+            connect.release(stmt, rs);
         }
     }
 
     //销量统计排行
-       //全局
-          //降序
-    public void selesVolumeOrderDesc(){
+    //全局
+    //降序
+    public void selesVolumeOrderDesc() {
         try {
             stmt = connect.getConnection().createStatement();
-            rs=stmt.executeQuery(  "select Product.proNum,proName,counts "+
-                    " from Product,"+
-                    " (select Shopping.proNum,sum(number) counts"+
-                   " from Shopping"+
-                    " group by proNum) AS PRO "+
-                   " where PRO.proNum=Product.proNum"+
+            rs = stmt.executeQuery("select Product.proNum,proName,counts " +
+                    " from Product," +
+                    " (select Shopping.proNum,sum(number) counts" +
+                    " from Shopping" +
+                    " group by proNum) AS PRO " +
+                    " where PRO.proNum=Product.proNum" +
                     " order by counts Desc;");
-            System.out.println("Product.proNum\t"+"proName\t\t\t"+"counts\t");
-            while(rs.next()){
-                System.out.println(rs.getString("Product.proNum")+"\t\t\t\t"+rs.getString("proName")+"\t"+rs.getString("counts")+"\t");
+            System.out.println("Product.proNum\t" + "proName\t\t\t" + "counts\t");
+            while (rs.next()) {
+                System.out.println(rs.getString("Product.proNum") + "\t\t\t\t" + rs.getString("proName") + "\t" + rs.getString("counts") + "\t");
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally{
-            connect.release(stmt,rs);
+        } finally {
+            connect.release(stmt, rs);
         }
     }
-    public void selesVolumeOrderAsc(){
+
+    public void selesVolumeOrderAsc() {
         try {
             stmt = connect.getConnection().createStatement();
-            rs=stmt.executeQuery(  "select Product.proNum,proName,counts "+
-                    "from Product,"+
-                    "(select Shopping.proNum,sum(number) counts"+
-                    "from Shopping"+
-                    "group by proNum) AS PRO "+
-                    "where PRO.proNum=Product.proNum"+
+            rs = stmt.executeQuery("select Product.proNum,proName,counts " +
+                    "from Product," +
+                    "(select Shopping.proNum,sum(number) counts" +
+                    "from Shopping" +
+                    "group by proNum) AS PRO " +
+                    "where PRO.proNum=Product.proNum" +
                     "order by counts Desc;");
-            System.out.println("Product.proNum\t"+"proName\t\t\t"+"counts\t");
-            while(rs.next()){
-                System.out.println(rs.getString("Product.proNum")+"\t\t\t\t"+rs.getString("proName")+"\t"+rs.getString("counts")+"\t");
+            System.out.println("Product.proNum\t" + "proName\t\t\t" + "counts\t");
+            while (rs.next()) {
+                System.out.println(rs.getString("Product.proNum") + "\t\t\t\t" + rs.getString("proName") + "\t" + rs.getString("counts") + "\t");
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally{
-            connect.release(stmt,rs);
+        } finally {
+            connect.release(stmt, rs);
         }
     }
 
     //订单录入
-    public void billInsert(){
+    public void billInsert() {
         try {
             stmt = connect.getConnection().createStatement();
-            stmt.executeUpdate("insert Shopping(billNum,proNum,number,price,shopDate) "+
-                    "values('S01','p1','2','10','2018-12-15'),"+
-                    "('S02','p3','1','3','2018-12-15'),"+
-                    "('S03','p5','3','6','2018-12-15'),"+
+            stmt.executeUpdate("insert Shopping(billNum,proNum,number,price,shopDate) " +
+                    "values('S01','p1','2','10','2018-12-15')," +
+                    "('S02','p3','1','3','2018-12-15')," +
+                    "('S03','p5','3','6','2018-12-15')," +
                     "('S04','p2','5','20','2018-12-15');");
-            System.out.println("Product.proNum\t"+"proName\t\t\t"+"counts\t");
-            while(rs.next()){
-                System.out.println(rs.getString("Product.proNum")+"\t\t\t\t"+rs.getString("proName")+"\t"+rs.getString("counts")+"\t");
+            System.out.println("Product.proNum\t" + "proName\t\t\t" + "counts\t");
+            while (rs.next()) {
+                System.out.println(rs.getString("Product.proNum") + "\t\t\t\t" + rs.getString("proName") + "\t" + rs.getString("counts") + "\t");
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             connect.release(stmt);
         }
     }
 
     //指定日期查询----降序
-    public void somedayCheack_Desc(){
+    public void somedayCheack_Desc() {
         try {
-            seResult=sql.select("Shopping.proNum,sum(number) counts","Shopping","shopDate='2018-12-15'"," group by proNum","");
-            result=sql.select("Product.proNum,proName,counts","Product,",seResult,"AS PRO","PRO.proNum=Product.proNum",""," order by counts Desc");
+            seResult = sql.select("Shopping.proNum,sum(number) counts", "Shopping", "shopDate='2018-12-15'", " group by proNum", "");
+            result = sql.select("Product.proNum,proName,counts", "Product,", seResult, "AS PRO", "PRO.proNum=Product.proNum", "", " order by counts Desc");
             stmt = connect.getConnection().createStatement();
-            rs=stmt.executeQuery(result);
-            System.out.println("Product.proNum\t"+"proName\t\t\t"+"counts\t");
-            while(rs.next()){
-                System.out.println(rs.getString("Product.proNum")+"\t\t\t\t"+rs.getString("proName")+"\t"+rs.getString("counts")+"\t");
+            rs = stmt.executeQuery(result);
+            System.out.println("Product.proNum\t" + "proName\t\t\t" + "counts\t");
+            while (rs.next()) {
+                System.out.println(rs.getString("Product.proNum") + "\t\t\t\t" + rs.getString("proName") + "\t" + rs.getString("counts") + "\t");
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally{
-            connect.release(stmt,rs);
-        }
-    }
-    //指定日期查询----升序
-    public void somedayCheack_Asc(){
-        try {
-            seResult=sql.select("Shopping.proNum,sum(number) counts","Shopping","shopDate='2018-12-15'"," group by proNum","");
-            result=sql.select("Product.proNum,proName,counts","Product,",seResult,"AS PRO","PRO.proNum=Product.proNum",""," order by counts Asc");
-            stmt = connect.getConnection().createStatement();
-            rs=stmt.executeQuery(result);
-            System.out.println("Product.proNum\t"+"proName\t\t\t"+"counts\t");
-            while(rs.next()){
-                System.out.println(rs.getString("Product.proNum")+"\t\t\t\t"+rs.getString("proName")+"\t"+rs.getString("counts")+"\t");
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }finally{
-            connect.release(stmt,rs);
+        } finally {
+            connect.release(stmt, rs);
         }
     }
 
-    //指定日期段查询
-    public void somedaysCheck(){
+    //指定日期查询----升序
+    public void somedayCheack_Asc() {
         try {
-            seResult=sql.select("  Shopping.proNum,sum(number) counts "," Shopping "," shopDate<='2018-12-15' and shopDate>='2018-12-13' "," group by proNum ","  ");
-            result=sql.select(" Product.proNum,proName,counts "," Product, ",seResult," AS PRO  ","  PRO.proNum=Product.proNum  ","  "," order by counts Desc ");
-            System.out.println(result);
+            seResult = sql.select("Shopping.proNum,sum(number) counts", "Shopping", "shopDate='2018-12-15'", " group by proNum", "");
+            result = sql.select("Product.proNum,proName,counts", "Product,", seResult, "AS PRO", "PRO.proNum=Product.proNum", "", " order by counts Asc;");
             stmt = connect.getConnection().createStatement();
-            rs=stmt.executeQuery(result);
-            System.out.println("Product.proNum\t"+"proName\t\t\t"+"counts\t");
-            while(rs.next()){
-                System.out.println(rs.getString("Product.proNum")+"\t\t\t\t"+rs.getString("proName")+"\t"+rs.getString("counts")+"\t");
+            rs = stmt.executeQuery(result);
+            System.out.println("Product.proNum\t" + "proName\t\t\t" + "counts\t");
+            while (rs.next()) {
+                System.out.println(rs.getString("Product.proNum") + "\t\t\t\t" + rs.getString("proName") + "\t" + rs.getString("counts") + "\t");
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally{
-            connect.release(stmt,rs);
+        } finally {
+            connect.release(stmt, rs);
         }
     }
+
+    //指定日期段查询----升序
+    public void somedaysCheck_Asc() {
+        try {
+            seResult = sql.select("  Shopping.proNum,sum(number) counts ", " Shopping ", " shopDate<='2018-12-15' and shopDate>='2018-12-13' ", " group by proNum ", "  ");
+            result = sql.select(" Product.proNum,proName,counts ", " Product, ", seResult, " AS PRO  ", "  PRO.proNum=Product.proNum  ", "  ", " order by counts Asc ;");
+            System.out.println(result);
+            stmt = connect.getConnection().createStatement();
+            rs = stmt.executeQuery(result);
+            System.out.println("Product.proNum\t" + "proName\t\t\t" + "counts\t");
+            while (rs.next()) {
+                System.out.println(rs.getString("Product.proNum") + "\t\t\t\t" + rs.getString("proName") + "\t" + rs.getString("counts") + "\t");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connect.release(stmt, rs);
+        }
+    }
+
+    //指定日期段查询----降序
+    public void somedaysCheck_Desc() {
+        try {
+            seResult = sql.select("  Shopping.proNum,sum(number) counts ", " Shopping ", " shopDate<='2018-12-15' and shopDate>='2018-12-13' ", " group by proNum ", "  ");
+            result = sql.select(" Product.proNum,proName,counts ", " Product, ", seResult, " AS PRO  ", "  PRO.proNum=Product.proNum  ", "  ", " order by counts Desc ;");
+            System.out.println(result);
+            stmt = connect.getConnection().createStatement();
+            rs = stmt.executeQuery(result);
+            System.out.println("Product.proNum\t" + "proName\t\t\t" + "counts\t");
+            while (rs.next()) {
+                System.out.println(rs.getString("Product.proNum") + "\t\t\t\t" + rs.getString("proName") + "\t" + rs.getString("counts") + "\t");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connect.release(stmt, rs);
+        }
+    }
+
+    //指定商品销量统计（比较）
+    //升序
+    public void productCompare_Asc() {
+        try {
+            seResult = sql.select(" Shopping.proNum,sum(number) counts ", " Shopping ", " group by proNum ", "  ");
+            result = sql.select(" Product.proNum,proName,counts ", " Product, ", seResult, " AS PRO ", " PRO.proNum=Product.proNum and Product.proNum in ('P1','P2') ", "  ", " order by counts ASC;");
+            System.out.println(result);
+            stmt = connect.getConnection().createStatement();
+            rs = stmt.executeQuery(result);
+            System.out.println("Product.proNum\t" + "proName\t\t\t" + "counts\t");
+            while (rs.next()) {
+                System.out.println(rs.getString("Product.proNum") + "\t\t\t\t" + rs.getString("proName") + "\t" + rs.getString("counts") + "\t");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connect.release(stmt, rs);
+        }
+    }
+
+    //降序
+    public void productCompare_Desc() {
+        try {
+            seResult = sql.select(" Shopping.proNum,sum(number) counts ", " Shopping ", " group by proNum ", "  ");
+            result = sql.select(" Product.proNum,proName,counts ", " Product, ", seResult, " AS PRO ", " PRO.proNum=Product.proNum and Product.proNum in ('P1','P2') ", "  ", " order by counts DESC;");
+            System.out.println(result);
+            stmt = connect.getConnection().createStatement();
+            rs = stmt.executeQuery(result);
+            System.out.println("Product.proNum\t" + "proName\t\t\t" + "counts\t");
+            while (rs.next()) {
+                System.out.println(rs.getString("Product.proNum") + "\t\t\t\t" + rs.getString("proName") + "\t" + rs.getString("counts") + "\t");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connect.release(stmt, rs);
+        }
+    }
+
+    //指定商品销量统计
+    public void productSell_Asc() {
+        try {
+            seResult = sql.select(" Shopping.proNum,sum(number) counts ", " Shopping "," group by proNum ", "  ");
+            result = sql.select(" Product.proNum,proName,counts ", " Product, ", seResult, " AS PRO ", " PRO.proNum=Product.proNum and PRO.proNum ", "  ", " order by counts ASC;");
+            System.out.println(result);
+            stmt = connect.getConnection().createStatement();
+            rs = stmt.executeQuery(result);
+            System.out.println("Product.proNum\t" + "proName\t\t\t" + "counts\t");
+            while (rs.next()) {
+                System.out.println(rs.getString("Product.proNum") + "\t\t\t\t" + rs.getString("proName") + "\t" + rs.getString("counts") + "\t");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connect.release(stmt, rs);
+        }
+    }
+
 }
--- src/SQL/SQLwords.java
+++ src/SQL/SQLwords.java
@@ -9,44 +9,48 @@ public class SQLwords {
 
 
     //select 方法
+    public String select(String targets, String tables, String grReult, String orResult) {
+        result = " select " + targets + " from " + tables  + grReult + orResult ;
+        return result;
+    }
     public String select(String targets, String tables,String conditions, String grReult, String orResult) {
-        result = " select " + targets + " from " + tables + " where " + conditions + grReult + orResult ;
+        result = " select " + targets + " from " + tables + " where " + conditions + grReult + orResult;
         return result;
     }
 
     public String select(String targets, String tables, String lowerResult, String as, String conditions, String grReult, String orResult) {
-        result = " select " + targets + " from " + tables +"("+ lowerResult +")"+ as + " where " + conditions + grReult + orResult + ";";
+        result = " select " + targets + " from " + tables +"("+ lowerResult +")"+ as + " where " + conditions + grReult + orResult ;
         return result;
     }
 
     //group方法
     public String group(String attributies) {
-        result = " group by " + attributies + ";";
+        result = " group by " + attributies;
         return result;
     }
 
     //order方法
     public String order(String attributies, String sortord) {
-        result = " order by " + attributies + " " + sortord + ";";
+        result = " order by " + attributies + " " + sortord ;
         return result;
     }
 
 
     //update方法
     public String update(String tables, String targets, String conditions) {
-        result = " update " + tables + " set " + targets + " where " + conditions + ";";
+        result = " update " + tables + " set " + targets + " where " + conditions ;
         return result;
     }
 
     //delete方法
     public String delete(String targets, String tables, String conditions) {
-        result = " delete " + targets + " from " + tables + " where " + conditions + ";";
+        result = " delete " + targets + " from " + tables + " where " + conditions ;
         return result;
     }
 
     //insert方法
     public String insert(String tables, String attributies, String data) {
-        result = " insert into " + tables + "(" + attributies + ") values " + data + ";";
+        result = " insert into " + tables + "(" + attributies + ") values " + data ;
         return result;
     }
 
