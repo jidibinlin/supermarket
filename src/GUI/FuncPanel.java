465769111905eeb05ca21f46af9ee067ffd54eeb
qibin <2694273649@qq.com>
2019-01-08 13:38:15 +0800

    table show before success

--- src/GUI/FuncPanel.java
+++ src/GUI/FuncPanel.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableColumnModel;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class FuncPanel {
@@ -24,6 +25,7 @@ public class FuncPanel {
 
     public void chartShowPanel(String tableColor){
             table .setTable(tableColor);
+            table.dateInput();
             funcScrollPanel[1].setViewportView(table.getTable());
     }
 
@@ -39,8 +41,10 @@ public class FuncPanel {
 
 class Table{
 
-    private JTable table =new JTable(40,10);
+    private JTable table =new JTable();
     private DefaultTableModel tableModel=new DefaultTableModel();
+    public Vector vDate=new Vector();
+    public Vector vTitle=new Vector();
 
     public JTable getTable(){
         return table;
@@ -52,6 +56,24 @@ class Table{
         Font f=new Font("Fonts/Go Mono for Powerline.ttf",Font.PLAIN,15);
         table.setBackground(color.getColor(tableColor));
         table.setFont(f);
+    }
+
+    public void dateInput(){
+        String[][] Date={{"李清照","29","女"},{"李清照","29","女"},{"李清照","29","女"},{"李清照","29","女"}};
+        String[] Title={"姓名","年龄","性别"};
+
+        for(int i=0;i<Date.length;i++) {
+            for (int j = 0; j < Title.length; j++) {
+                vDate.add(Date[i][j]);
+            }
+        }
+        for(int i=0;i<Title.length;i++){
+            vTitle.add((String)Title[i]);
+        }
+        tableModel.setDataVector(vDate,vTitle);
+        table.setModel(tableModel);
+        table.validate();
+        table.updateUI();
     }
 
     public void delete(){
