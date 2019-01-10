ea344db55088eaafb7df8078c5baf768273571e1
zyz <756259772@qq.com>
2019-01-10 11:48:33 +0800

    谁谁谁

--- src/SQL/EmployeeManage.java
+++ src/SQL/EmployeeManage.java
@@ -2,8 +2,68 @@ package SQL;
 
 import JDBC.connect;
 import java.sql.*;
+import java.util.Vector;
 
 public class EmployeeManage {
-    Statement stmt=null;
-    ResultSet rs=null;
+    Statement stmt = null;
+    ResultSet rs = null;
+    String seResult;
+    String result;
+    String update_result;
+    SQLwords sql = new SQLwords();
+    int rowCount, columCount;
+    int i, j;
+    Vector data = new Vector();
+    Vector title = new Vector();
+    Vector[] resu = {data, title};
+    Vector[] sql_result;
+
+    public Vector[] sql_result(String sqlName) {
+
+        switch (sqlName) {
+
+        }
+        return sql_result;
+    }
+    public String update_result(String sqlName) {
+        switch (sqlName) {
+
+        }
+        return update_result;
+    }
+
+
 }
+
+
+
+
+//    private Vector[] ()
+//
+//    {
+//        try {
+//            data.removeAllElements();
+//            title.removeAllElements();
+//            seResult = sql.select("  ", "  ", "  ", "  ", "  ");
+//            result = sql.select("  ", " , ", seResult, "  ", " ; ", "  ", "  ");
+//            stmt = connect.getConnection().createStatement();
+//            rs = stmt.executeQuery(result);
+//            title.add("");
+//            title.add("");
+//            title.add("");
+//            while (rs.next()) {
+//                Vector row = new Vector();
+//                row.add(rs.getString(""));
+//                row.add(rs.getString(""));
+//                row.add(rs.getString(""));
+//                data.add(row);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            connect.release(stmt, rs);
+//        }
+//        return resu;
+//
+//    }
+//
