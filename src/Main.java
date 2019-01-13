b78fa7a36271dbac689f8ca032f3ea003bf0f2a7
zyz <756259772@qq.com>
2019-01-13 12:27:52 +0800

    在main。Java文件里直接调用update函数

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 import GUI.GUI;
+import SQL.BasicTableSelect;
 import SQL.FinanceManage;
 
 public class Main {
     public static void main(String[] args) {
         GUI g = new GUI();
+        String A;
         g.integrate();
+        BasicTableSelect b= new BasicTableSelect();
+        A=b.Update("Manu","manuName='超大宝'","manuNum='M04'");
+        System.out.println(A);
     }
 }
 
